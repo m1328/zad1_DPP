481493dc5b31a4186f4570d3a4fd9b2fcee22973
Maria Okuniewicz <okuniewicz13@gmail.com>
2025-10-12 18:33:55 +0200

    Finishing methods [#86c5yhuhq]

--- metody.py
+++ metody.py
@@ -35,12 +35,41 @@ def calculate_discount(price, discount):
 
 print(calculate_discount(100,0.2))
 
-# funkcja 5 (do poprawy):
+# funkcja 5:
 def flatten_list(nested_list):
-    new_list = []
-    for num in nested_list:
-        new_list.append(num)
-    return new_list
+    flat_list = []
+    for item in nested_list:
+        if isinstance(item, list):
+            flat_list += flatten_list(item)
+        else:
+            flat_list.append(item)
+    return flat_list
 
 print(flatten_list([1, [2, 3], [4, [5]]]))
+
+# funkcja 6:
+def word_frequencies(text):
+    text = text.lower()
+    text_cleaned = ''.join(ch if ch.isalpha() or ch.isspace() else ' ' for ch in text)
+
+    words_dict = {}
+    for word in text_cleaned.split():
+        words_dict[word] = words_dict.get(word, 0) + 1
+    return words_dict
+
+print(word_frequencies("Tekst, tekst! Ala ma kota. Ala? KOTA!!! Ala ala"))
+
+#funkcja 7:
+def is_prime(n):
+    if n<2:
+        return False
+    for i in range(2,n):
+        if n % i ==0:
+            return False
+    return True
+
+print(is_prime(8))
+print(is_prime(9))
+print(is_prime(3))
+
 
